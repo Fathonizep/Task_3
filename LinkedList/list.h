f71a44b357e1ce0203b85de6f4712dc933fe61c3
Fathoni Zeptian Eka P <fathoniian@gmail.com>
2015-09-21 16:06:51 +0700

    Update list.h
    Fathoni Zeptian Eka P (1301144286)
    IF 38 06

--- LinkedList/list.h
+++ LinkedList/list.h
@@ -1,6 +1,55 @@
-#ifndef LIST_H_INCLUDED
-#define LIST_H_INCLUDED
+#ifndef ASD_H_INCLUDED
+#define ASD_H_INCLUDED
+#include <iostream>
+#include <stdlib.h>
+#include <cstring>
+#include <conio.h>
 
 
+#define nil NULL
+#define info(p) (p)->info
+#define next(p) (p)->next
+#define first(l) ((l).first)
 
-#endif // LIST_H_INCLUDED
+
+struct infotype
+{
+    char id[6];
+    char nama[15];
+    char golongan[10];
+
+
+};
+
+typedef struct elmlist *address;
+
+struct elmlist
+{
+    infotype info;
+    address next;
+};
+
+struct list
+{
+    address first;
+};
+
+void createlist (list *l);
+void inputdata (infotype *x);
+void dealokasi (address p);
+address alokasi (infotype x);
+
+void insertfirst (list *l, address p);
+void insertlast (list *l, address p);
+void insertafter (list *l, address p, address prec);
+
+void deletefirst (list *l, address *p);
+void deletelast (list *l, address *p);
+void deleteafter (list *l, address *p, address prec);
+
+address findelm(list l, infotype x);
+void viewlist (list l);
+
+
+#endif // ASD_H_INCLUDED
+
