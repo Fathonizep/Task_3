ed0b41acbda00dd5b5ddcd6434ff889894748120
shasaorryza <shasaorryza13@gmail.com>
2015-09-21 17:31:48 +0700

    Update list.cpp
    Shasa Oryza IF 38 06

--- LinkedList/list.cpp
+++ LinkedList/list.cpp
@@ -115,3 +115,48 @@ void deletefirst (list *l, address *p)
     }
 }
 
+void deletelast (list *l, address *p)
+{
+    address q;
+
+    if (first(*l) == nil)
+    {
+        cout<<"List belum tersedia";
+    }
+    else if (next(first(*l)) == nil)
+    {
+        *p=first(*l);
+        first(*l) = nil;
+        dealokasi(*p);
+    }
+    else
+    {
+        q=first(*l);
+        while (next(next(q)) != nil)
+        {
+            q=next(q);
+        }
+        *p = next(q);
+        next(q) = nil;
+        dealokasi(*p);
+    }
+}
+
+void insertlast (list *l, address p)
+{
+    address q;
+
+    if (first(*l) == nil)
+    {
+        first(*l) = p;
+    }
+    else
+    {
+        q = first(*l);
+        while (next(q) != nil)
+        {
+            q = next(q);
+        }
+        next(q) = p;
+    }
+}
